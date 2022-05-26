14bdb0274709d2aae987b75afbe173579498e4fc
jaylaelike <smarkwisai@gmail.com>
2022-05-26 16:32:28 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
 import { createClient } from "next-sanity";
 import ButtonAppBar from "../components/ButtonAppBar";
